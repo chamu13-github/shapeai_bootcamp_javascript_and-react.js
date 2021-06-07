d46e6639a991a94881773a946d91db4d69a5895c
chamu13-github <79440526+chamu13-github@users.noreply.github.com>
2021-06-06 23:35:43 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>React js</h1>
+      <p>123... abcd...... ABCD....
+      </p>
+    </div>
+  );
+}
+
+export default Note;
